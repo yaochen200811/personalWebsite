0c8430f46082c4d3f359d0b9a701b7476997c63a
Chen Yao <yaochen200811@gmail.com>
2023-04-05 17:10:54 -0700

    update profiles

--- src/component/Project.tsx
+++ src/component/Project.tsx
@@ -14,16 +14,31 @@ const Project = ({ setPage, displayPage }: ProjectProps) => {
 	return (
 		<>
 			<ContentContainer displayPage={displayPage}>
-				<h2>LD48- Treasure | 2021.4</h2>
+				<h2>LD48- Treasure | April 2021</h2>
 				<a href="https://ldjam.com/events/ludum-dare/48/treasure">website</a> <br /> <br />
 				<li>A game developed for Ludum Dare 48, 72-hour game development Jam. The game was developed with one teammate. All the code and assets were completed in 72 hours</li>
 				<li>Developed with Unity as the game engine, graphic assets drawn with Adobe PS, and sound effects made with sfxr. Handled problems such as 2D sprites shader, and web server setup</li>
 				<br />
-				<h2>SlideChat | 2020.8</h2>
+				<h2>Distributed Scalable URL Shortener | University of Toronto | CSC409 | 2020 Fall</h2>
+				<li>High performance, high availability, load balancing, disaster recovery, horizontally and vertically scalable server that redirect user from the server generated shorted URL to the actual URL</li>
+				<li>Tech stack: Java, Docker Swarm, Cassandra</li>
+				<br />
+				<h2>Image Super-resolution | University of Toronto | CSC420 | 2020 Fall</h2>
+				<li>PyTorch implementation of modern example-based super-resolution methods: SRCNN, SRResNet, SRGAN; and quality metrics: PSNR, SSIM</li>
+				<li>Analysis of pros and cons of each method</li>
+				<br />
+				<h2>SlideChat | University of Toronto | CSC492 | 2020 Summer</h2>
 				<a href="https://github.com/larryyueli/slidechat">github</a> <br /> <br />
-				<li>Slidechat is an online content publishing platform used by professors and students at U of T</li>
-				<li>Allows Professors to upload course slides with audios, and students can comment on each page</li>
-				<li>Developed with MongoDB, Express, NodeJS, React and various web developing tools and libraries</li>
+				<li>Slidechat is an open-source teaching resource publishing platform designed for professors and students</li>
+				<li>Used by over 400 students and professors in University of Toronto</li>
+				<li>Professors able to upload course slides with audios, and students can comment on each page</li>
+				<li>Tech stack: MongoDB, Express, React, NodeJS, Socket.IO, Apache</li>
+				<br />
+				<h2>Neo Deerhunt Coding Competition hack lab | March 2019</h2>
+				<a href="https://www.facebook.com/utmmcss/posts/2146231848798870">facebook</a> <br /> <br />
+				<li>Hackathon competition held at University of Toronto Mississauga</li>
+				<li>Result: Team won first place in the competition of more than ten teams. Designed and coded AI for characters in a PVP game</li>
+				<li>Methodology: Used Python to implement methods for characters in the game to beat opponents</li>
 				{BackButton(() => setPage("home"))}
 			</ContentContainer>
 		</>
--- src/component/Work.tsx
+++ src/component/Work.tsx
@@ -14,13 +14,22 @@ const Work = ({ setPage, displayPage }: WorkProps) => {
 	return (
 		<>
 			<ContentContainer displayPage={displayPage}>
-				<h2>FULL-STACK SOFTWARE DEVELOPER | MANORLEAD | 2021.5 – PRESENT</h2>
+				<h2>FULL-STACK SOFTWARE DEVELOPER | Manorlead | Richmond Hill, ON | May 2021 - Present </h2>
 				<a href="https://www.manorlead.com">website</a> <br /> <br />
-				<li>Working as the major developer of web front-end. Website built with JavaScript, React, and various web developing tools and libraries. Features include location maps, 3D models, and AI real estate price prediction · Helps with website design and gives back-end suggestions </li>
-				<li>Lead developer of a mobile AR APP. Build with Unity AR with geolocation, surface detection and more </li>
-				<li>Lead developer of a openAI based WhatsApp and website chatbot. Features include helping customers find residential listings, making appointments, and getting notifications </li>
-				<li>Actively participated in team meetings to update the workflow to contribute to the progress of the project </li>
-				<li>Earned positive feedback from the team manager about my strong work ethic </li>
+				<li>Working as the major developer of web front-end. Website building with JavaScript, React, and various libraries. Features include location maps, 3D models, and AI real estate price prediction </li>
+				<li>Made improvements that increased the website home page loading speed by 20%</li>
+				<li>Engaging in website design and giving back-end suggestions</li>
+				<li>Introducing the website structure to interns. Answering their questions when they are working on tasks</li>
+				<li>Building mobile AR APP with Unity AR with geolocation, surface detection as the lead developer</li>
+				<li>Managing the openAI based WhatsApp and website chatbot project. Features include helping customers find residential listings,making appointments, and getting notifications</li>
+				<li>Actively participate in team meetings to update the workflow to contribute to the progress of the project.</li>
+				<li>Earned positive feedback from the team manager about my strong work ethic</li>
+				<br />
+				<h2>Customer Support Agent | Qianmi | Jiangsu, China | May 2017 – July 2017</h2>
+				<li>Communicated with customers to get the requirements of their ideal home page then built and maintained the home page with the Qianmi WeChat Mini Programs</li>
+				<li>Designed event posters for customers using Adobe Photoshop </li>
+				<li>Addressed the needed new features and improvable points to developers and discussed which points are worth to implement and how to achieve the goal</li>
+				<li>Serviced a total of over five customers and earned positive feedback</li>
 				{BackButton(() => setPage("home"))}
 			</ContentContainer>
 		</>
